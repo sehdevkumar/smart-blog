4deb8558f2982153b41d10567f646db4d7099b73
sehdev <sehdev@atai.ai>
2024-06-25 18:50:13 +0530

    rich text done

--- src/app/(features)/writepost/page.tsx
+++ src/app/(features)/writepost/page.tsx
@@ -3,7 +3,7 @@ import RichTextEditor from '../../components/TextEditor'
 
 function WritePostPage() {
   return (
-    <div className='grid h-[calc(100%-0px)] w-full p-11'>
+    <div className='grid relative h-[calc(100%-0px)] w-[calc(100%-300px)] m-auto p-20'>
       <RichTextEditor/>
     </div>
   )
--- src/app/components/TextEditor.tsx
+++ src/app/components/TextEditor.tsx
@@ -1,99 +1,142 @@
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-"use client";
-import { AddIcon } from '@chakra-ui/icons';
-import { Image } from '@chakra-ui/react';
-import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+'use client'
+import { AddIcon, SmallCloseIcon } from '@chakra-ui/icons'
+import { Image } from '@chakra-ui/react'
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 const toolbarOptions = [
-  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['bold', 'italic', 'underline', 'strike'], // toggled buttons
   ['blockquote', 'code-block'],
   ['link', 'image', 'video', 'formula'],
 
-  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-  [{ 'direction': 'rtl' }],                         // text direction
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+  [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+  [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+  [{ direction: 'rtl' }], // text direction
 
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
-  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
-  [{ 'align': [] }],
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
 
-  ['clean']                                 // remove formatting button
-];
-
-
+  ['clean'], // remove formatting button
+]
 
 interface RichTextEditorRef {
-  getValue: () => string;
-  setValue: (value: string) => void;
-  clear: () => void;
+  getValue: () => string
+  setValue: (value: string) => void
+  clear: () => void
 }
 
+export default forwardRef<RichTextEditorRef, {}>(function RichTextEditor(
+  props,
+  ref,
+) {
+  const [value, setValue] = useState('')
+  const editorRef = useRef<any>(null)
+  const toolBarRef = useRef<any>(null)
+  const toolbarIconRef = useRef<any>(null)
+  const storeCoordsRef = useRef<number[]>([])
+  const [getIconToggled, setIconToggled] = useState<boolean>(false)
 
-
-export default forwardRef<RichTextEditorRef,{}>(function RichTextEditor(props,ref) {
-
-
- const [value, setValue] = useState('');
- const editorRef = useRef<any>(null);
- const toolBarRef = useRef<any>(null);
- const toolbarIconRef = useRef<any>(null);
-
-
-   useImperativeHandle(ref, () => ({
+  useImperativeHandle(ref, () => ({
     getValue: () => value,
     setValue: (newValue) => setValue(newValue),
     clear: () => setValue(''),
-  }));
-   
+  }))
 
-   useEffect(() => {
+  const toolBarIconHandle = () => {
+    if (!getIconToggled) {
+      const toolbar = toolBarRef.current as HTMLDivElement
+      if (toolbar) {
+        toolbar.style.display = 'block'
+        toolbar.style.zIndex = '9999999999999'
+        toolbar.style.position = 'absolute'
+        // toolbar.style.left = `${(storeCoordsRef?.current[0])}px`
+        toolbar.style.top = `${storeCoordsRef?.current[1]}px`
+      }
+      setIconToggled(true)
+    } else {
+      const toolbar = toolBarRef.current as HTMLDivElement
+      if (toolbar) {
+        toolbar.style.zIndex = '9999999999999'
+        toolbar.style.position = 'absolute'
+        // toolbar.style.left = `${(storeCoordsRef?.current[0])}px`
+        toolbar.style.top = `${storeCoordsRef?.current[1]}px`
+      }
+      toolbar.style.display = 'none'
+      setIconToggled(false)
+    }
+  }
+
+  useEffect(() => {
     if (editorRef.current) {
-      const editorElement = editorRef.current.getEditor().root;
+      const editorElement = editorRef.current.getEditor().root
       // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
-      const toolbar = document.querySelector('.ql-toolbar')!;
+      const toolbar = document.querySelector('.ql-toolbar')!
       toolBarRef.current = toolbar
       editorElement.addEventListener('pointerdown', (e: PointerEvent) => {
-        const x = e.clientX;
-        const y = e.clientY;
-        // if (toolbar) {
-        //   toolbar.style.position = 'absolute';
-        //   toolbar.style.left = `${x}px`;
-        //   toolbar.style.top = `${y}px`;
-        // }
-      });
+        e?.stopPropagation()
+
+        const toolbar = toolBarRef.current as HTMLDivElement
+        
+        const toolbarIcon = toolbarIconRef.current as HTMLDivElement
+        const x = e.clientX
+        const y = e.pageY
+
+        storeCoordsRef.current = [x, y - 80]
+        toolbar.style.top = `${storeCoordsRef?.current[1]}px`
+        if (toolbarIcon) {
+          toolbarIcon.style.position = 'absolute'
+          toolbarIcon.style.left = `25px`
+          toolbarIcon.style.top = `${y - 80}px`
+        }
+      })
     }
 
     return () => {
       if (editorRef.current) {
-        const editorElement = editorRef.current.getEditor().root;
+        const editorElement = editorRef.current.getEditor().root
         editorElement.removeEventListener('pointerdown', (e: PointerEvent) => {
           // Cleanup logic
-        });
+        })
       }
-    };
-  }, [editorRef]);
-
-
-  
-
+    }
+  }, [editorRef])
 
   return (
-    <>
-       <div>
-         <AddIcon/>
-       </div>
-       <ReactQuill ref={editorRef} modules={{toolbar: toolbarOptions}} placeholder='Tell Your Story...' theme="snow" value={value} onChange={setValue} />;
-    
-    </>
-  ) 
-  
-}
-)
+    <div className="">
+      <div
+        onClick={toolBarIconHandle}
+        ref={toolbarIconRef}
+        className="z-[9999999999] cursor-pointer border-slate-800 border-2 w-[40px] h-[40px] rounded-full flex justify-center items-center"
+      >
+        {getIconToggled && <SmallCloseIcon />}
+        {!getIconToggled && <AddIcon />}
+      </div>
+      <div>
+        <ReactQuill
+          ref={editorRef}
+          modules={{ toolbar: toolbarOptions }}
+          placeholder="Tell Your Story..."
+          theme="snow"
+          value={value}
+          onChange={setValue}
+        />
+        ;
+      </div>
+    </div>
+  )
+})
