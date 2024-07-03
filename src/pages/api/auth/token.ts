8d894e223634e69c2f3ebd0d35c1d284115f8fb2
sehdev <sehdev@atai.ai>
2024-07-03 18:10:21 +0530

    message changes

--- src/pages/api/auth/token.ts
+++ src/pages/api/auth/token.ts
@@ -17,9 +17,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         jwt.verify(token, process.env.REFRESH_TOKEN_SECRET!, (err: any, decodedUser: any) => {
             if (err) return res.status(403);
 
-            const accessToken = generateAccessToken({ id: user.id, email: user.email });
+            const accessToken = generateAccessToken({
+                id: user.id, email: user.email,
+                name: user.name ?? ''
+            });
             res.json({ accessToken });
         });
+
+      return res.status(403);
+
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
